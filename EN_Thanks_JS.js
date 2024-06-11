69588c3e95b800aaec1b05a9cddd5e6ef096cb3b
lishaykroy <l13497l@gmail.com>
2024-06-11 16:54:54 +0300

    added new branch

---
+++ EN_Thanks_JS.js
@@ -0,0 +1,48 @@
+/* Clear Storage On Refresh */
+
+    window.addEventListener('beforeunload', function() {
+        localStorage.clear();
+    });
+
+/* Cursor Changes */
+
+    const cursor = document.querySelector('.cursor');
+    var timeout;
+
+    document.addEventListener("mousemove", (e) => {
+        let x = e.pageX;
+        let y = e.pageY;
+
+        cursor.style.top = y + "px";
+        cursor.style.left = x + "px";
+        cursor.style.display = "block";
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+            cursor.style.display = "none";
+        }, 1000);
+
+        if (e.target.tagName !== 'A' && e.target.tagName !== 'I') {
+            cursor.style.display = "block";
+        } else if (e.target.classList.contains('logo')) {
+            cursor.style.display = "block";
+        } else {
+            cursor.style.display = "none";
+        }
+    });
+
+    document.addEventListener("mouseout", () => {
+        cursor.style.display = "none";
+    });
+
+/* Use Full Name Of Form Submitter */
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var fullName = localStorage.getItem('fullName');
+
+        if (fullName) {
+            document.getElementById('fullNameSpan').innerText = fullName;
+        }
+    });
---
+++ HE_Main_Loading_JS.js
@@ -0,0 +1,21 @@
+/* Cursor Changes */
+
+const cursor = document.querySelector('.cursor');
+var timeout;
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+    cursor.style.display = "block";
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+        cursor.style.display = "none";
+    }, 1000);
+    
+});
+
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+});
